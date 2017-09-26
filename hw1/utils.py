0a4329e66b883e57a617d1e34274f25be61247a3
Hugh Krogh-Freeman <hk2903@columbia.edu>
2017-09-26 05:34:20 +0000

    deleted buffering code

--- hw1/utils.py
+++ hw1/utils.py
@@ -7,19 +7,6 @@ Created on Sun Sep 24 13:40:16 2017
 import sys
 import os
 
-class Unbuffered(object):
-   def __init__(self, stream):
-       self.stream = stream
-   def write(self, data):
-       self.stream.write(data)
-       self.stream.flush()
-   def writelines(self, datas):
-       self.stream.writelines(datas)
-       self.stream.flush()
-   def __getattr__(self, attr):
-       return getattr(self.stream, attr)
-sys.stdout = Unbuffered(sys.stdout)
-
 from sklearn.feature_selection import SelectKBest, mutual_info_classif
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
