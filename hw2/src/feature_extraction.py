c838bd21b1f30274f481c5f8c2c831ceb0dacf36
Hugh Krogh-Freeman <hk2903@columbia.edu>
2017-10-14 10:02:19 -0700

    renamed variables

--- hw2/src/feature_extraction.py
+++ hw2/src/feature_extraction.py
@@ -10,54 +10,54 @@ def get_features(config,sent_dict):
     # distance between 2nd element on stack and second element in the buffer
     if len(config[0]) > 1 and len(config[1]) > 1:
         distance = config[0][-2] - config[1][-2]
-        features.append('SEC_DISTANCE_STK_BUFF_' + str(distance))
+        features.append('SECOND_DISTANCE_STK_BUFF_' + str(distance))
 
     # first word in the buffer
     if len(config[1]) > 0:
         features.append('TOP_BUFF_TOKEN_'+str(sent_dict['FORM'][config[1][-1]].lower()))
         # POS of first word in the buffer
-        features.append('TOP_BUFF_POS_'+str(sent_dict['CPOSTAG'][config[1][-1]]))
+        features.append('TOP_BUFF_CPOSTAG_'+str(sent_dict['CPOSTAG'][config[1][-1]]))
 
     # second word in the buffer
     if len(config[1]) > 1:
         # token
-        features.append('SEC_BUFF_TOKEN_'+str(sent_dict['FORM'][config[1][-2]].lower()))
+        features.append('SECOND_BUFF_TOKEN_'+str(sent_dict['FORM'][config[1][-2]].lower()))
         # POS 
-        features.append('SEC_BUFF_POS_'+str(sent_dict['CPOSTAG'][config[1][-2]]))
-        features.append('SEC_BUFF_POSTAG_'+str(sent_dict['POSTAG'][config[1][-2]]))
+        features.append('SECOND_BUFF_CPOSTAG_'+str(sent_dict['CPOSTAG'][config[1][-2]]))
+        features.append('SECOND_BUFF_POSTAG_'+str(sent_dict['POSTAG'][config[1][-2]]))
 
     # third word in the buffer
     if len(config[1]) > 2:
         # token
         features.append('THIRD_BUFF_TOKEN_'+str(sent_dict['FORM'][config[1][-3]].lower()))
         # POS
-        features.append('THIRD_BUFF_POS_'+str(sent_dict['CPOSTAG'][config[1][-3]]))
+        features.append('THIRD_BUFF_CPOSTAG_'+str(sent_dict['CPOSTAG'][config[1][-3]]))
 
     # fourth word in the buffer    
     if len(config[1]) > 3:
         # token
         features.append('FOURTH_BUFF_TOKEN_'+str(sent_dict['FORM'][config[1][-4]].lower()))
         # POS
-        features.append('FOURTH_BUFF_POS_'+str(sent_dict['CPOSTAG'][config[1][-4]]))
+        features.append('FOURTH_BUFF_CPOSTAG_'+str(sent_dict['CPOSTAG'][config[1][-4]]))
 
     # top of stack word
     if len(config[0]) > 0:
         features.append('TOP_STK_TOKEN_'+str(sent_dict['FORM'][config[0][-1]].lower()))
-        features.append('TOP_STK_POS_' + sent_dict['CPOSTAG'][config[0][-1]])
+        features.append('TOP_STK_CPOSTAG_' + sent_dict['CPOSTAG'][config[0][-1]])
         features.append('TOP_STK_LEMMA_'+str(sent_dict['LEMMA'][config[0][-1]].lower())) 
         features.append('TOP_STK_POSTAG_' + sent_dict['POSTAG'][config[0][-1]])
         
     # 2nd item from top of stack
     if len(config[0]) > 1:
         # Top 2nd item of the stack
-        features.append('TOP2_STK_TOKEN_'+str(sent_dict['FORM'][config[0][-2]].lower()))
-        features.append('TOP2_STK_POS_'+str(sent_dict['CPOSTAG'][config[0][-2]]))
+        features.append('SECOND_STK_TOKEN_'+str(sent_dict['FORM'][config[0][-2]].lower()))
+        features.append('SECOND_STK_CPOSTAG_'+str(sent_dict['CPOSTAG'][config[0][-2]]))
 
     # 3rd item from top of stack
     if len(config[0]) > 2:
         # Top 3rd item of the stack
-        features.append('TOP3_STK_LEMMA_'+str(sent_dict['LEMMA'][config[0][-3]].lower()))    
-        features.append('TOP3_STK_POS_'+str(sent_dict['CPOSTAG'][config[0][-3]]))    
+        features.append('THIRD_STK_LEMMA_'+str(sent_dict['LEMMA'][config[0][-3]].lower()))    
+        features.append('THIRD_STK_CPOSTAG_'+str(sent_dict['CPOSTAG'][config[0][-3]]))    
 
     features.append('NOUN_COUNT_' + str(sent_dict['CPOSTAG'].count('NOUN')))
     features.append('PUNCT_COUNT_' + str(sent_dict['CPOSTAG'].count('PUNCT')))
@@ -67,12 +67,12 @@ def get_features(config,sent_dict):
 
     features.append('SYM_IND_' + str(sent_dict['POSTAG'].count('SYM')))
     features.append('VBZ_IND_' + str(sent_dict['POSTAG'].count('VBZ')))
-    features.append('VBN_IND_' + str(sent_dict['POSTAG'].count('VBN')))    
+    features.append('VBN_IND_' + str(sent_dict['POSTAG'].count('VBN')))
     features.append('WDT_IND_' + str(sent_dict['POSTAG'].count('WDT')))
     features.append('WP_IND_' + str(sent_dict['POSTAG'].count('WP')))
     features.append('WRB_IND_' + str(sent_dict['POSTAG'].count('WRB')))
     
-    features.append('WP_POS_IND_' + str(sent_dict['POSTAG'].count('WP$')))
+    features.append('WP_CPOSTAG_IND_' + str(sent_dict['POSTAG'].count('WP$')))
     features.append('FW_IND_' + str(sent_dict['POSTAG'].count('FW')))
     features.append('LS_IND_' + str(sent_dict['POSTAG'].count('LS')))
     features.append('UH_IND_' + str(sent_dict['POSTAG'].count('UH')))
